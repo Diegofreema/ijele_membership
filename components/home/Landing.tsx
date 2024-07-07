49a18a471fa76dda61dee2ab6e548959afd412ba
Diego <diegofreeman78@gmail.com>
2024-07-07 22:14:50 +0100

    removed the overlay

--- components/home/Landing.tsx
+++ components/home/Landing.tsx
@@ -20,15 +20,6 @@ export const Landing = () => {
       </Box>
 
       <CaptionText />
-      <Box
-        position={'absolute'}
-        top={0}
-        bottom={0}
-        right={0}
-        zIndex={2}
-        left={0}
-        backgroundColor={'rgba(0,0,0,0.5)'}
-      />
     </Box>
   );
 };
